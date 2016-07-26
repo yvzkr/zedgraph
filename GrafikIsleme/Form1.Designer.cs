bf2aae70ad29bea4079047d7bb000150f9361766
Yavuz Kuru <yavuzkuru3@gmail.com>
2016-07-26 13:14:51 +0300

    first
    zedgraph base using

---
+++ GrafikIsleme/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace GrafikIsleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            resources.ApplyResources(this.zedGraphControl1, "zedGraphControl1");
+            this.zedGraphControl1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.zedGraphControl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.zedGraphControl1.IsAutoScrollRange = true;
+            this.zedGraphControl1.IsEnableVZoom = false;
+            this.zedGraphControl1.IsScrollY2 = true;
+            this.zedGraphControl1.IsShowPointValues = true;
+            this.zedGraphControl1.IsSynchronizeXAxes = true;
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            this.zedGraphControl1.ZoomEvent += new ZedGraph.ZedGraphControl.ZoomEventHandler(this.zedGraphControl1_ZoomEvent);
+            this.zedGraphControl1.ScrollEvent += new System.Windows.Forms.ScrollEventHandler(this.zedGraphControl1_ScrollEvent);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GrafikIsleme/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace GrafikIsleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SecilenGrafik();      
+        }
+        int TickStart;
+        string ScaleMod;
+        Random rastgele = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            int sayi = rastgele.Next(50, 150);
+            
+            Draw(sayi);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "Bobin";
+            myPane.XAxis.Title.Text = "Zaman";
+            myPane.YAxis.Title.Text = "Derece";
+            // buradaki 60000 i 5 yada başka bir şey yaparsak ekranda gözüken
+            //çizginin boyu 5 birim oluyor.
+            RollingPointPairList list = new RollingPointPairList(60000);
+            RollingPointPairList list2 = new RollingPointPairList(60000);
+            LineItem curve = myPane.AddCurve("List", list, Color.Blue, SymbolType.None);
+
+            //bunlar grafiğin bayutlarını ayarlar
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 180;
+            myPane.YAxis.Scale.Min = 0;
+            myPane.YAxis.Scale.Max = 250;
+            
+
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 5;
+            zedGraphControl1.AxisChange();
+            TickStart = Environment.TickCount;
+            
+
+        }
+        private void SecilenGrafik()
+        {
+            double[] x = new double[180];
+            double[] y = new double[400];
+            int i;
+           
+            
+            for (i = 0; i <x.Length; i++)
+            {
+                
+                x[i] = i;
+                y[i] = i;
+            }
+           
+            //msj = sayi % 2 == 0 ? "çift sayi girdiniz" : "Tek sayi girdiniz";
+            //koşul? doğru ifade: yanlış ifade
+            for ( i = 10; i < 61; i++)
+            {
+                y[i] = 200;
+            }
+            for (i = 60; i < 121; i++)
+            {
+                y[i] = 150;
+            }
+            for (i = 120; i < 180; i++)
+            {
+                y[i] = 200;
+            }
+
+             
+            zedGraphControl1.GraphPane.AddCurve("Sinus Dalgası", x, y, Color.Red, SymbolType.None);
+            //myPane.AddCurve("Sinus Dalgası", x, y, Color.Red, SymbolType.None);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+
+        }
+        private void Draw(double yekseni)
+        {
+            zedGraphControl1.AutoScroll = true;
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+            {
+                return;
+            }
+            LineItem curve1 = zedGraphControl1.GraphPane.CurveList[1] as LineItem;
+            if (curve1 == null)
+            {
+                return;
+            }
+            IPointListEdit list1 = curve1.Points as IPointListEdit;
+            if (list1 == null)
+            {
+                return;
+            }
+
+            double time = (Environment.TickCount - TickStart) / 1000.0;
+            list1.Add(time, yekseni);
+           // Scale xScale = zedGraphControl1.GraphPane.XAxis.Scale;
+            //if (time>xScale.Max-xScale.MajorStep)
+            //{
+            //    if (intMode==1)
+            //    {
+            //        xScale.Max = time + xScale.MajorStep;
+            //        xScale.Min = xScale.Max - 30.0;
+
+            //    }
+            //    else
+            //    {
+            //        xScale.Max = time + xScale.MajorStep;
+            //        xScale.Min = 0;
+            //    }
+            //}
+            Scale xScale = zedGraphControl1.GraphPane.XAxis.Scale;
+            if (ScaleMod=="takipet")
+            {
+                xScale.Max = 180;
+                //xScale.Min = xScale.Max - 200;
+            }
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+
+        }
+        private void ScaleAyari() {
+            
+            zedGraphControl1.GraphPane.XAxis.Scale.Min = 0;
+            zedGraphControl1.GraphPane.XAxis.Scale.Max = 180;
+            zedGraphControl1.GraphPane.YAxis.Scale.Min = 0;
+            zedGraphControl1.GraphPane.YAxis.Scale.Max = 250;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "TAKİP ET")
+            {
+                ScaleAyari();
+                ScaleMod = "takipet";
+                button1.Text = "SERBEST MODE";
+            }
+            else if(button1.Text=="SERBEST MODE")
+            {
+                ScaleAyari();
+                ScaleMod = "serbest";
+                button1.Text = "TAKİP ET";
+            }
+
+        }
+
+        private void zedGraphControl1_ScrollEvent(object sender, ScrollEventArgs e)
+        {
+            //zedGraphControl1.GraphPane.XAxis.Scale.Min = 0;
+        }
+
+        private void zedGraphControl1_ZoomEvent(ZedGraphControl sender, ZoomState oldState, ZoomState newState)
+        {
+            //if (zedGraphControl1.GraphPane.XAxis.Scale.Min<0)
+            //{
+            //    zedGraphControl1.GraphPane.XAxis.Scale.Min = 0;
+            //}
+            
+        }
+    }
+}
